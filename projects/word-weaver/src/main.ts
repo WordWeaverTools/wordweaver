3c0aeda4b6f4a2a692ec05390809f74ba967b31e
Serge Leger <Serge.Leger@nrc-cnrc.gc.ca>
2026-01-30 14:19:02 -0400

    chore: replaced deprecated angular modules

--- projects/word-weaver/src/main.ts
+++ projects/word-weaver/src/main.ts
@@ -1,7 +1,7 @@
-import { enableProdMode, provideZoneChangeDetection } from "@angular/core";
-import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { enableProdMode } from "@angular/core";
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
+import { platformBrowser } from "@angular/platform-browser";
 
 if (environment.production) {
   enableProdMode();
@@ -19,8 +19,6 @@ if (environment.production && environment.plausibleAnalyticsDataDomain) {
   head.appendChild(analysticsScript);
 }
 
-platformBrowserDynamic()
-  .bootstrapModule(AppModule, {
-    applicationProviders: [provideZoneChangeDetection()],
-  })
+platformBrowser()
+  .bootstrapModule(AppModule, {})
   .catch((err) => console.error(err));
