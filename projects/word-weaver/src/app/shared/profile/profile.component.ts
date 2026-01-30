0ec93d36ad7b77966cc0293b67aaaa4746d2f748
Serge Leger <Serge.Leger@nrc-cnrc.gc.ca>
2026-01-30 07:42:51 -0400

    chore(deps): automated update to angular 20

--- projects/word-weaver/src/app/shared/profile/profile.component.ts
+++ projects/word-weaver/src/app/shared/profile/profile.component.ts
@@ -1,6 +1,5 @@
-import { Component, Inject } from "@angular/core";
+import { Component, Inject, DOCUMENT } from "@angular/core";
 import { AuthService } from "@auth0/auth0-angular";
-import { DOCUMENT } from "@angular/common";
 
 @Component({
   selector: "ww-app-user-profile",
